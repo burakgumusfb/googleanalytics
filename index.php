98aa5895962ce9345ffbea4d1ea69a6f1f654567
Burak Gümüş <burakgumusfb@gmail.com>
2019-02-18 21:17:52 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-134747229-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'UA-134747229-1');
+</script>
+
 <?php
 
 echo "hi";
